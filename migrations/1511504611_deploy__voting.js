8684c486defe18009b4a6437374f987ffafdd575
Jim <james.w.belford@gmail.com>
2017-11-26 12:02:48 -0800

    Added candidates for test deploy

--- migrations/1511504611_deploy__voting.js
+++ migrations/1511504611_deploy__voting.js
@@ -2,5 +2,5 @@ const Voting = artifacts.require("./Voting.sol")
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
-  deployer.deploy(Voting)
+  deployer.deploy(Voting,['Bob','Alice'],{gas: 6700000});
 };
